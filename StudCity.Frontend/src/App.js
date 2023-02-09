81cfc55c32304da107cffed42948a6230dfadb60
Kinas Maria <kinasmaria01@gmail.com>
2023-02-09 21:01:56 +0100

    Email confirmation template

--- StudCity.Frontend/src/App.js
+++ StudCity.Frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
+
                 <Counter />
                 <p className="text-3xl font-bold underline">
                     Edit <code>src/App.js</code> and save to reload.
