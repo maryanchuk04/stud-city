301d1d7c35e4c94257596b8bfc64dd5c6a50e052
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-02-10 15:11:52 +0200

    Authenticate page create

--- StudCity.Frontend/tailwind.config.js
+++ StudCity.Frontend/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'md': '0 3px 10px rgba(0, 0, 0, 0.2)',
+      }
+    }
   },
   plugins: [],
 }
