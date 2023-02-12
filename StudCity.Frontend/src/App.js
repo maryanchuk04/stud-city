d8b8b91ce4e290de1468a035be7ceb41de58f7ad
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-02-12 21:29:07 +0200

    create registration page

--- StudCity.Frontend/src/App.js
+++ StudCity.Frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom"
 import Authenticate from "./pages/authenticate/Authenticate";
+import Registration from "./pages/registration/Registration";
+
 function App() {
     return (
         <div className="App">
-           <Authenticate/>
+            <Routes>
+                <Route path="authenticate" element = { <Authenticate /> } />
+                <Route path="registration" element = { <Registration /> } />
+            </Routes>
         </div>
     );
 }
--- StudCity.Frontend/src/UI/Button.jsx
+++ StudCity.Frontend/src/UI/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ children, className = "", disabled = false }) {
     return (
        <button  
-            className={`rounded-2xl bg-primaryGreen disabled:cursor-not-allowed text-primatyWhite mt-3 h-16 w-1/1 font-normal ${className}`} 
+            className={`rounded-2xl bg-primaryAuthentication disabled:cursor-not-allowed text-primatyWhite mt-3 h-16 w-1/1 font-normal text-xl ${className}`} 
             disabled = { disabled } 
             type = "submit"
         >
