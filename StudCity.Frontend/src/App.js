867d7aab7992833e7cf566dd14178059d8f9a779
Kinas Maria <kinasmaria01@gmail.com>
2023-02-16 18:51:22 +0100

    create submit email page

--- StudCity.Frontend/src/App.js
+++ StudCity.Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"
 import Authenticate from "./pages/authenticate/Authenticate";
 import Registration from "./pages/registration/Registration";
+import VerifyEmail from "./pages/verify-email/VerifyEmail";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="authenticate" element = { <Authenticate /> } />
                 <Route path="registration" element = { <Registration /> } />
+                <Route path="verify-email/:acountId" element = { <VerifyEmail /> } />
             </Routes>
         </div>
     );
