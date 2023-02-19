e95a1d532e607a0e608f90e6aff8d91a23f8dde1
Kinas Maria <kinasmaria01@gmail.com>
2023-02-19 17:25:08 +0100

    fixed comments

--- StudCity.Frontend/src/App.js
+++ StudCity.Frontend/src/App.js
@@ -7,7 +7,7 @@ import {
 import Main from "./pages/Main";
 import Authenticate from "./pages/authenticate";
 import Registration from "./pages/registration";
-import VerifyEmail from "./pages/verify-email/VerifyEmail";
+import VerifyEmail from "./pages/verify-email";
 
 const router = createBrowserRouter([
 	{
