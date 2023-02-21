0bdb6864c2c66a8b47c47ae648d182e9615236e8
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-02-21 18:34:43 +0200

    fix h-44

--- StudCity.Frontend/src/UI/Avatar/index.jsx
+++ StudCity.Frontend/src/UI/Avatar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Avatar({ src, className = "" }) {
 	return(
-		<div className={`rounded-full h-[180px] mx-auto w-[180px] overflow-hidden ${className}`}>
+		<div className={`rounded-full h-44 mx-auto w-44 overflow-hidden ${className}`}>
 				<img src={src} className="w-full h-full object-contain" alt="" />
 		</div>
 	);
