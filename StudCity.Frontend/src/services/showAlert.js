35002f84fb440d658034fe5f40558a7e067a0d10
Serhiy Dvoryannikov <65117661+Serhiy-dotcom@users.noreply.github.com>
2023-02-23 12:41:10 +0200

    create simple show alert function (#65)

---
+++ StudCity.Frontend/src/services/showAlert.js
@@ -0,0 +1,22 @@
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
+
+const notyf = new Notyf({
+	ripple: false,
+	dismissible: true,
+	position: {
+		x: 'center',
+		y: 'top'
+	},
+	types: [
+		{
+			type: 'info',
+			background: '#F4D690',
+			duration: 4000
+		}
+	]
+});
+
+export const showAlert = (message = '', type = 'success') => {
+	notyf.open({ type, message });
+};
