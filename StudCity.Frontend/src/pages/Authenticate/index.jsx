5f60c09371f26ba50ee58dc944dc91903b379e85
Maks <lion20914king@gmail.com>
2023-02-27 21:51:35 +0200

    Fix conflicts

--- StudCity.Frontend/src/pages/Authenticate/index.jsx
+++ StudCity.Frontend/src/pages/Authenticate/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthenticateService } from "../../services/authenticateService";
 import { emailValidator } from "../../utils/validators/validators";
 import Label from "../../UI/Label";
