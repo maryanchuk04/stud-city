9726dae4c0357512b08ca930817b49e1a64fe1eb
mohammed0xff <mohammed0xff@gamil.com>
2023-03-04 22:04:04 +0200

    add `UserDto`

---
+++ StudCity.Backend/StudCity.Core/DTOs/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using StudCity.Core.Enums;
+
+namespace StudCity.Core.DTOs;
+public class UserDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string FullName { get; set; }
+
+    public string UserName { get; set; }
+
+    public DateTime DateOfBirthday { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Gender Gender { get; set; }
+
+}
+
