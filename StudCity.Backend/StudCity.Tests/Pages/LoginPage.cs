1ec1daa8393bdea13a0c00b3e7050a0ea4fc328c
Maks Maryanchuk <lion20914king@gmail.com>
2023-03-28 17:40:14 +0300

    Write some automated tests for UI with Playwright

---
+++ StudCity.Backend/StudCity.Tests/Pages/LoginPage.cs
@@ -0,0 +1,30 @@
+using Microsoft.Playwright;
+
+namespace StudCity.Tests.Pages;
+
+public class LoginPage
+{
+    private readonly IPage _page;
+    private readonly ILocator _btnLogin;
+    private readonly ILocator _txtEmail;
+    private readonly ILocator _txtPassword;
+
+    public LoginPage(IPage page)
+    {
+        _page = page;
+        _btnLogin = _page.Locator("text=Submit");
+        _txtEmail = _page.GetByPlaceholder("Enter your email");
+        _txtPassword = _page.GetByPlaceholder("Enter your password");
+    }
+
+    public async Task ClickLogin() => await _btnLogin.ClickAsync();
+
+    public async Task<bool> FormIsValid() => !await _btnLogin.IsDisabledAsync();
+
+    public async Task Login(string email, string password)
+    {
+        await _txtEmail.FillAsync(email);
+        await _txtPassword.FillAsync(password);
+        await ClickLogin();
+    }
+}
