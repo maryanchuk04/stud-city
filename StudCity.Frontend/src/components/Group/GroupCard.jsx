26780c28ac4c18b5a6c403c0008d3811e50c7069
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-07-02 14:06:58 +0300

    fixed step choose groups

--- StudCity.Frontend/src/components/Group/GroupCard.jsx
+++ StudCity.Frontend/src/components/Group/GroupCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import GridCard from './GridCard'
 import ListCard from './ListCard'
 
-const GroupCard = ({ group, handleClick, viewMode }) => {
+const GroupCard = ({ group, handleClick, isActive, viewMode }) => {
 
 	const stylesContainerCardMode = {
 		grid: "duration-200 hover:scale-90 h-60",
@@ -16,9 +16,12 @@ const GroupCard = ({ group, handleClick, viewMode }) => {
 
 	return (
 		<div className={`shadow-form flex w-full rounded-xl relative cursor-pointer overflow-hidden ${stylesContainerCardMode[viewMode]}`}
-			onClick={() => handleClick()}
+			onClick={() => handleClick(group.id)}
 		>
 			{renderCardMode[viewMode]}
+			{isActive && <div className='rounded-xl absolute bg-[#453e354d] top-0 left-0 w-full h-full flex'>
+				<i className="m-auto z-20 text-white text-6xl fa-solid fa-badge-check"></i>
+			</div>}
 		</div>
 	)
 }
